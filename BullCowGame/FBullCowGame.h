cf6b81c2dbe18461da47b3c06f519a9c37f86565
Saphronov Mark <hijarian@gmail.com>
2017-01-22 18:06:06 +0300

    Добавил FBullCowGame.h согласно уроку 24

---
+++ BullCowGame/FBullCowGame.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class FBullCowGame
+{
+public:
+	void Reset();
+	int GetMaxTries();
+	int GetCurrentTry();
+	bool IsGameWon();
+	bool CheckGuessValidity(std::string);
+};
